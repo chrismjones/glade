05b94e2655922112828f4468b74ba249ca392d6e
Carlos Fernandez <carlos.fernandez@whro.org>
2024-05-09 12:46:53 -0400

    [#187152815] Modifying intercomMessenger() to delete script tag when fired

--- plugins/addIntercomMessenger.js
+++ plugins/addIntercomMessenger.js
@@ -1,39 +1,47 @@
 import onNavigate from '../lib/onNavigate';
 
 function intercomMessenger() {
-  window.intercomSettings = { app_id: "sqvgjxhh"};
-  (function () {  
-    var w = window; 
-    var ic = w.Intercom; 
-    if (typeof ic === "function") { 
-      ic('reattach_activator'); 
-      ic('update', w.intercomSettings); 
-    } 
-    else { 
-      var d = document; 
-      var i = function () { 
-        i.c(arguments); }; 
-        i.q = []; 
-        i.c = function (args) { 
-          i.q.push(args); }; 
-          w.Intercom = i; 
-          var l = function () { 
-            var s = d.createElement('script'); 
-            s.type = 'text/javascript'; 
-            s.async = true; 
-            s.src = 'https://widget.intercom.io/widget/sqvgjxhh'; 
-            var x = d.getElementsByTagName('script')[0]; 
-            x.parentNode.insertBefore(s, x); 
-          }; 
-          if (w.attachEvent) { 
-            w.attachEvent('onload', l); } 
-            else { w.addEventListener('load', l, false); 
-          } 
-        } 
-      })();
+  window.intercomSettings = { app_id: "sqvgjxhh" };
+  
+  (function() {
+      var w = window;
+      var ic = w.Intercom;
+      if (typeof ic === "function") {
+          ic('reattach_activator');
+          ic('update', w.intercomSettings);
+      } else {
+          var d = document;
+          var i = function() {
+              i.c(arguments);
+          };
+          i.q = [];
+          i.c = function(args) {
+              i.q.push(args);
+          };
+          w.Intercom = i;
+          var l = function() {
+              var s = d.createElement('script');
+              s.type = 'text/javascript';
+              s.async = true;
+              s.src = 'https://widget.intercom.io/widget/sqvgjxhh';
+              s.setAttribute('id', 'intercom-messenger') 
+              var x = d.getElementsByTagName('script')[0];
+              x.parentNode.insertBefore(s, x);
+          };
+          if (document.readyState !== 'complete') {
+              w.addEventListener('load', l, false);
+          } else {
+              l();
+          }
+      }
+  })();
 }
 
 export default () => {
   intercomMessenger();
-  onNavigate(() => intercomMessenger());
+  onNavigate(() => {
+    window.Intercom = null;
+    document.getElementById('intercom-messenger').remove();
+    intercomMessenger();
+  });
 }
