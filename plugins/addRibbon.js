13f2d99988b13d5a4b1b43ea08012e7ce60de90b
Carlos Fernandez <carlos.fernandez@whro.org>
2024-06-12 12:12:57 -0400

    [#187390546] Creating separate urls for pledge drive images

--- plugins/addRibbon.js
+++ plugins/addRibbon.js
@@ -96,7 +96,8 @@ function addRibbon() {
     
     	if(ribbonDateRange.contains(ribbonNow)) {
             var htm = "";
-            var pledgeImgs = "https://pages.whro.org/assets/imgs/";
+            var commonImgs = "https://pages.whro.org/assets/imgs/pledge/"
+            var pledgeImgs = "https://pages.whro.org/assets/imgs/pledge/202406/";
 
             const today = new Date();
             const day = today.getDate().toString().padStart(2, "0");
@@ -105,20 +106,20 @@ function addRibbon() {
             const formattedDate = `${month}${day}${year}`; //Output example: 052424
 
 	   		htm += '<style>';
-	    	htm += '.pledgeBanner{ background:url("'+pledgeImgs+'swyl_bkg_corpBlue.jpg") repeat-x transparent 0 0; height:90px; }';
+	    	htm += '.pledgeBanner{ background:url("'+commonImgs+'swyl_bkg_corpBlue.jpg") repeat-x transparent 0 0; height:90px; }';
 			htm += '.pledgeBanner{ background-color:#0b4f71; height:90px; }';
 			htm += '.pledgeBanner .siteBannerInner{ height:90px; justify-content:space-between; }';
 			htm += '	@media (max-width:750px){ .pledgeBanner .siteBannerInner { justify-content:space-around; } }';
-			htm += '.pledgeBanner .mdAmounts a{ width:216px; height:90px; background:url("'+pledgeImgs+'btn_donateNow_Off.png") no-repeat transparent 0 0; display:inline-block; text-indent:-9000px; }';
-			htm += '.pledgeBanner .mdAmounts a:hover{ background:url("'+pledgeImgs+'btn_donateNow_On.png") no-repeat transparent 0 0; }';
-			htm += '@media(max-width: 853px) {.pledgeBanner .smAmounts a{ width:103px; height:90px; background:url("'+pledgeImgs+'btn_donateNow_sq_Off.png") no-repeat transparent 0 0; display:inline-block; text-indent:-9000px; }}';
-			htm += '.pledgeBanner .smAmounts a:hover{ background:url("'+pledgeImgs+'btn_donateNow_sq_On.png") no-repeat transparent 0 0; }';
+			htm += '.pledgeBanner .mdAmounts a{ width:216px; height:90px; background:url("'+commonImgs+'btn_donateNow_Off.png") no-repeat transparent 0 0; display:inline-block; text-indent:-9000px; }';
+			htm += '.pledgeBanner .mdAmounts a:hover{ background:url("'+commonImgs+'btn_donateNow_On.png") no-repeat transparent 0 0; }';
+			htm += '@media(max-width: 853px) {.pledgeBanner .smAmounts a{ width:103px; height:90px; background:url("'+commonImgs+'btn_donateNow_sq_Off.png") no-repeat transparent 0 0; display:inline-block; text-indent:-9000px; }}';
+			htm += '.pledgeBanner .smAmounts a:hover{ background:url("'+commonImgs+'btn_donateNow_sq_On.png") no-repeat transparent 0 0; }';
 			htm += '	.pledgeBanner .mdAmounts{ line-height:1.5em; padding: 25px 0; }';
 			htm += '</style>';
 	        htm += '<div class="siteBanner pledgeBanner">';
 			htm += 		'<div class="container siteBannerInner d-flex">';
-			htm += 			'<img class="bannerLogo d-none d-md-block" src="'+pledgeImgs+'swylHeart_l.png" alt="" />';
-			htm += 			'<img class="bannerLogo d-md-none" src="'+pledgeImgs+'swylHeart_s.png" alt="" />';
+			htm += 			'<img class="bannerLogo d-none d-md-block" src="'+commonImgs+'swylHeart_l.png" alt="" />';
+			htm += 			'<img class="bannerLogo d-md-none" src="'+commonImgs+'swylHeart_s.png" alt="" />';
 			htm += 			'<img id="img_599" class="d-none" src="'+pledgeImgs+formattedDate+'_599.png" alt="" />';
 			htm += 			'<img id="img_250" class="d-none" src="'+pledgeImgs+formattedDate+'_250.png" alt="" />';
 			htm += 			'<div class="amounts d-none d-md-block mdAmounts">';
