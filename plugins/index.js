aac3c5b947e148e21c4cfb50b4ff3c783a030227
Christopher Jones <chris.jones@whro.org>
2024-07-10 13:37:05 -0400

    [#187935950] 🔥 Removing unused plugins from St Louis Public Media

--- plugins/index.js
+++ plugins/index.js
@@ -1,16 +1,11 @@
 import { default as addIntercomMessenger }  from './addIntercomMessenger';
 import { default as addRibbon            }  from './addRibbon';
 import { default as addTiktokSocialIcon  }  from './addTiktokSocialIcon'
-import { default as addUMSLAttribution   }  from './addUMSLAttribution'
-import { default as animateLogos         }  from './animateLogos'
 import { default as applyThemeByTag      }  from './applyThemeByTag'
 import { default as identifyCurrentPage  }  from './identifyCurrentPage'
 import { default as formatTranscripts    }  from './formatTranscripts'
 import { default as googleCustomSearch   }  from './googleCustomSearch'
-import { default as loadDonateAsk        }  from './loadDonateAsk'
-import { default as loadDonatePopUp      }  from './loadDonatePopUp'
 import { default as Placeholder          }  from './components.Placeholder'
-import { default as replaceNewsletter    }  from './replaceNewsletter'
 
 
 const LGBTTheme = `
@@ -36,23 +31,13 @@ const plugins = [
   { fn: addIntercomMessenger, enabled: true  },
   { fn: addRibbon,            enabled: true  },
   { fn: addTiktokSocialIcon,  enabled: true  },
-  { fn: addUMSLAttribution,   enabled: false },
-  { fn: animateLogos,         enabled: false },
   { fn: identifyCurrentPage,  enabled: true  },
   { fn: formatTranscripts,    enabled: true  },
   { fn: googleCustomSearch,   enabled: false },
-  { fn: loadDonateAsk,        enabled: false },
-  { fn: loadDonatePopUp,      enabled: false },
-  { fn: Placeholder,          enabled: true },
-  { fn: applyThemeByTag,       enabled: false,
+  { fn: Placeholder,          enabled: true  },
+  { fn: applyThemeByTag,      enabled: false,
     args: [['LGBT', 'LGBTQ'], LGBTTheme],
-  },
-  { fn: replaceNewsletter,  enabled: false,
-    args: ['STL Welcome Kit',
-      'https://app.e2ma.net/app2/audience/signup/1931618/1784312/', // The Gateway
-      'https://app.e2ma.net/app2/audience/signup/1992160/1784312/', // STL Welcome Kit
-    ],
-  },
+  }
 ]
 
 
